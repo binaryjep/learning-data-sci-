acd5a63517551afbbac347b7f1fb4fc2ae30819b
binaryjep <jeffescueta13@gmail.com>
2025-01-16 20:32:02 +0800

    binomial

---
+++ practice/binomial_distribution.py
@@ -0,0 +1,10 @@
+import numpy as np 
+from scipy.stats import binom
+
+np.random.seed(10)
+
+# Simulate 52 weeks of 3 deals 
+deals = binom.rvs(3, 0.3, size=52)
+
+# Print mean deals won per week 
+print(np.mean(deals))
